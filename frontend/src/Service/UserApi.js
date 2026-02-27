3fcb2ffc80ebf9072cf01f189f12abe738322f9d
Asmi Gupta <asmigupta.mca24@bvicam.in>
2026-02-27 15:39:54 +0530

    Update Profile and Settings components in backend

--- frontend/src/Service/UserApi.js
+++ frontend/src/Service/UserApi.js
@@ -11,3 +11,5 @@ export const getInterestedUsers = () => api.get("/api/users/interested");
 
 
 export const getNotSubscribedUsers= () => api.get("/api/users/not-subscribed");
+
+export const updateCurrentUser = (userData) => api.put("/api/users/profile", userData);
